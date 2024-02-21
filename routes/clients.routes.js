b2b76562c1b2116038b310f0ee2f41ce43a5de6e
Distrib-PS <theodelhay@orange.fr>
2024-02-21 14:21:04 +0100

    oups

--- routes/clients.routes.js
+++ routes/clients.routes.js
@@ -6,8 +6,8 @@ export const router = express.Router();
 router.get('', clientsController.getAllHotel());
 router.get('/rooms', clientsController.getAllRooms());
 router.get('/rooms/:id', clientsController.getClientById());
-router.post('/clients/:clientId/rooms/:roomId/reserve', clientsController.reserveRoom());
-router.delete('/clients/:clientId/cancel-reservation', clientsController.cancelReservation());
+router.post('/rooms/:clientId/rooms/:roomId/reserve', clientsController.reserveRoom());
+router.delete('/rooms/:clientId/cancel-reservation', clientsController.cancelReservation());
 
 
 
